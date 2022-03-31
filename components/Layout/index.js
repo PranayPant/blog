379fe3f4d3ccd1ae04d94852d19be82be9ddfe3b
PranayPant <ppantg1@gmail.com>
2022-03-30 21:32:35 -0500

    editor formating

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,13 +1,15 @@
-import Navbar from "@components/Navbar";
+import Navbar from '@components/Navbar'
 
-export default function Layout({children}){
-    return <div>
-    <main className='h-screen'>
-      <h1 className='text-center text-6xl font-bold py-10'>
-        Young Gunz Boxing
-      </h1>
-        <Navbar />
-      {children}
-    </main>
-  </div>
+export default function Layout({ children }) {
+   return (
+      <div>
+         <main className="h-screen">
+            <h1 className="text-center text-6xl font-bold py-10">
+               Young Gunz Boxing
+            </h1>
+            <Navbar />
+            <div className="h-full flex flex-col">{children}</div>
+         </main>
+      </div>
+   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,17 @@
 import Head from 'next/head'
 
 export default function Home() {
-  return (
-    <div>
-      <Head>
-      <title>Young Gunz Boxing</title>
-      <meta name="description" content="Boxing club dedicated to boxing fundamentals, home of peekaboo boxing" />
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-      <h2>Home Page</h2>
-    </div>
-  )
+   return (
+      <>
+         <Head>
+            <title>Young Gunz Boxing</title>
+            <meta
+               name="description"
+               content="Boxing club dedicated to boxing fundamentals, home of peekaboo boxing"
+            />
+            <link rel="icon" href="/favicon.ico" />
+         </Head>
+         <h2>Home Page</h2>
+      </>
+   )
 }
